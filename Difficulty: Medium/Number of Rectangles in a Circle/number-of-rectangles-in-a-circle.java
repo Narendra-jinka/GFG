36b999ec9e79f787580415388005cd9fb17515b1
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2024-06-18 20:29:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Rectangles in a Circle/number-of-rectangles-in-a-circle.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            int ans = ob.rectanglesInCircle(N);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int rectanglesInCircle(int r) {
+        // code here
+       // if(r<=1) return r;
+        
+        int limit =4*r*r;
+        int ans =0;
+        for(int i=1;i<=2*r;i++)
+        {
+            for(int j=1;j<=2*r;j++)
+            {
+                if( i*i + j*j <=limit)ans++;
+            }
+        }
+        return ans;
+        
+        
+    }
+};
