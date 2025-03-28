334f7436c4a1531c8378065f08c5c9f4e1a8df74
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2025-03-28 23:26:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Activity Selection/activity-selection.java
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -32,29 +32,23 @@ public class Main {
 
 // } Driver Code Ends
 
-
-
-
-
-
 class Solution {
-    public int activitySelection(int[] start, int[] finish) {
-        // code here.
-         int n=start.length;
+    public int activitySelection(int[] nums1, int[] nums2) {
+        int n=nums1.length;
        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
-            arr[i][0] = finish[i];
-            arr[i][1] = start[i];
+            arr[i][0] = nums2[i];
+            arr[i][1] = nums1[i];
         }
          Arrays.sort(arr,(a, b) -> Integer.compare(a[0], b[0]));
-         int fin=-1;
+         int finish=-1;
          int ans=0;
          for(int i=0;i<arr.length;i++){
-             if(arr[i][1]>fin){
+             if(arr[i][1]>finish){
                  ans++;
-                 fin=arr[i][0];
+                 finish=arr[i][0];
              }
          }
          return ans;
     }
-} 
+}
